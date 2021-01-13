03f0dc25f11277b4f1da61fa8ea4e0bc71ba0093
sanqi777 <50070756+sanqi777@users.noreply.github.com>
2021-01-14 00:05:32 +0800

    摆动序列.md

--- src/com/san/Algorithm/最长摆动序列_376.java
+++ src/com/san/Algorithm/最长摆动序列_376.java
@@ -6,6 +6,34 @@ package com.san.Algorithm;
  */
 public class 最长摆动序列_376 {
     public static int wiggleMaxLength(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return n;
+        }
+        int[] up = new int[n];
+        int[] down = new int[n];
+        up[0] = down[0] = 1;
+        for (int i = 2; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up[i] = Math.max(up[i - 1], down[i - 1] + 1);
+                down[i] = down[i - 1];
+            } else if (nums[i] < nums[i - 1]) {
+                up[i] = up[i - 1];
+                down[i] = Math.max(down[i - 1], up[i - 1] + 1);
+            } else {
+                up[i] = up[i - 1];
+                down[i] = down[i - 1];
+            }
+        }
+        return Math.max(up[n-1],down[n-1]);
+    }
+
+    /**
+     * 优化
+     * @param nums
+     * @return
+     */
+    public int wiggleMaxLength2(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
